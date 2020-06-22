d89bcb3a1ad994f1f8cf7f1919e19f3832fc0507
Robert Cereghini <rcereghini@gmail.com>
2020-06-22 09:34:03 -0700

    Comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { v1 as uuid } from "uuid";
 import "./App.css";
 
 function App() {
+  //mockAuth is intended to be an authentication placeholder.
+  //This allows firebase to remember who you are without having real auth in place.
+  //Remove the mockAuth value from localStorage to begin as a new user.
+  //Copy and paste mockAuth if you'd like to revisit that "acccount" after erasing.
+
   if (!localStorage.getItem("mockAuth")) {
     localStorage.setItem("mockAuth", uuid());
     firestore
